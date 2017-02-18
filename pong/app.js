a61c07e4eec9700b50c8d10c82970f28f4a24f1f
WolfieZero <neil@wolfiezero.com>
2017-02-18 22:31:54 +0000

    Close #2, close #3, close #4

--- pong/app.js
+++ pong/app.js
@@ -2,31 +2,126 @@ class Pong {
 
     constructor() {
         this.context = null;
-        this.createCanvas();
-       // this.createPaddle();
-    }
-
-    createCanvas(width = 480, height = 320) {
-        console.log('create canvas');
-        const canvas = document.createElement('canvas');
-        canvas.id = 'pong';
-        canvas.width = width;
-        canvas.height = height;
-        document.body.appendChild(canvas);
-        this.context = canvas.getContext('2d');
-        this.context.fillStyle = "#222222";
-        this.context.fillRect(25, 25, 100, 100);
-    }
-
-    createPaddle() {
-        if (this.context !== null) {
-            console.log('create paddle');
-            this.context.beginPath();
-            this.context.fillRect(25, 25, 100, 100);
+        this.config = {
+            stage: {
+                width: 480,
+                height: 320,
+                color: '#222222',
+            },
+            paddle: {
+                width: 5,
+                height: 80,
+                color: '#efefef',
+            },
+            ball: {
+                size: 10
+            }
         }
+    }
+
+    init() {
+        this.createStage(this.config.stage.width, this.config.stage.height).then(context => {
+
+            const ball = new Ball(
+                context,
+                5,
+                this.config.stage.width / 2,
+                this.config.stage.height / 2
+            ).draw();
+
+            const player = new Paddle(
+                context,
+                this.config.paddle.width,
+                this.config.paddle.height,
+                20,
+                (this.config.stage.height - this.config.paddle.height) / 2,
+            ).draw();
+
+            const ai = new Paddle(
+                context,
+                this.config.paddle.width,
+                this.config.paddle.height,
+                this.config.stage.width - 20,
+                (this.config.stage.height - this.config.paddle.height) / 2,
+            ).draw();
+
+        });
+    }
+
+    createStage(width, height) {
+        return new Promise((resolve, reject) => {
+            try {
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+                document.body.appendChild(canvas);
+                canvas.id = 'pong';
+                canvas.width = width;
+                canvas.height = height;
+                context.fillStyle = this.config.stage.color;
+                context.fillRect(0, 0, width, height);
+                resolve(context);
+            }
+            catch(error) {
+                reject('Cannot create stage: ' + error);
+            }
+        });
     }
 
 }
 
 
-window.onload = new Pong;
+class Ball {
+
+    constructor(context, size, x, y, color = '#efefef') {
+        this.color = color;
+        this.context = context;
+        this.xPosition = x;
+        this.yPosition = y;
+        this.size = size;
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.fillStyle = this.color;
+        this.context.arc(
+            this.xPosition,
+            this.yPosition,
+            this.size,
+            2 * Math.PI,
+            false
+        );
+        this.context.fill();
+    }
+
+}
+
+
+class Paddle {
+
+    constructor(context, width, height, x, y, color = '#efefef') {
+        this.color = color;
+        this.context = context;
+        this.height = height;
+        this.width = width;
+        this.xPosition = x;
+        this.yPosition = y;
+    }
+
+    draw() {
+        this.context.beginPath();
+        this.context.fillStyle = this.color;
+        this.context.fillRect(
+            this.xPosition,
+            this.yPosition,
+            this.width,
+            this.height
+        );
+    }
+
+}
+
+
+window.onload = () => {
+    const pong = new Pong;
+    pong.init();
+}
